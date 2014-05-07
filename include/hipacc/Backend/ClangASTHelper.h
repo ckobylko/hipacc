35a2cfd3ee1190d995c1cd56168f937cc86c70d6
Christof Kobylko <ckobylko@users.noreply.github.com>
2014-05-07 11:20:42 +0200

    Exported class CLangASTHelper into an own source => make it usable for the whole Backend library

---
+++ include/hipacc/Backend/ClangASTHelper.h
@@ -0,0 +1,177 @@
+//
+// Copyright (c) 2012, University of Erlangen-Nuremberg
+// Copyright (c) 2012, Siemens AG
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// 1. Redistributions of source code must retain the above copyright notice, this
+//    list of conditions and the following disclaimer.
+// 2. Redistributions in binary form must reproduce the above copyright notice,
+//    this list of conditions and the following disclaimer in the documentation
+//    and/or other materials provided with the distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+// ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+// ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+// ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+
+//===--- ClangASTHelper.h - Implements helper class for easy clang AST handling. -----===//
+//
+// This file implements a helper class which contains a few methods for easy clang AST handling.
+//
+//===---------------------------------------------------------------------------------===//
+
+#ifndef _BACKEND_CLANG_AST_HELPER_H_
+#define _BACKEND_CLANG_AST_HELPER_H_
+
+#include "hipacc/AST/ASTNode.h"
+#include <string>
+
+namespace clang
+{
+namespace hipacc
+{
+namespace Backend
+{
+  /** \brief  Helper class which contains a few methods for easy clang AST handling. */
+  class ClangASTHelper final
+  {
+  public:
+
+    typedef ::llvm::SmallVector< ::clang::Stmt*, 16U >  StatementVectorType;  //!< Type definition for a vector of statements.
+
+  private:
+
+    ::clang::ASTContext   &_rCtx;   //!< A reference to the current AST context.
+
+
+    ClangASTHelper(const ClangASTHelper &) = delete;
+    ClangASTHelper& operator=(const ClangASTHelper &) = delete;
+
+  public:
+
+    /** \brief  Constructor.
+      *  \param  rAstContext   A reference to the current AST context. */
+    ClangASTHelper(::clang::ASTContext &rAstContext) : _rCtx(rAstContext)   {}
+
+    /** \brief  Returns a reference to the current AST context. */
+    inline ::clang::ASTContext& GetASTContext()   { return _rCtx; }
+
+    /** \brief  Returns the corresponding pointer type for a qualified clang type.
+      *  \param  crPointeeType   A reference to the qualified type whose pointer type shall be returned. */
+    inline ::clang::QualType    GetPointerType(const ::clang::QualType &crPointeeType)  { return GetASTContext().getPointerType(crPointeeType); }
+
+
+    /** \name AST node creation methods */
+    //@{
+
+    /** \brief  Creates an subscript expression.
+      *  \param  pArrayRef         A pointer to a declaration reference expression of the array.
+      *  \param  pIndexExpression  A pointer to the expression object, which returns the index of the subscript.
+      *  \param  crReturnType      The return type of the array subscript.
+      *  \param  bIsLValue         Specifies, whether the array subscript expression is used as a L-value of another expression. */
+    ::clang::ArraySubscriptExpr*  CreateArraySubscriptExpression(::clang::DeclRefExpr *pArrayRef, ::clang::Expr *pIndexExpression, const ::clang::QualType &crReturnType, bool bIsLValue = false);
+
+    /** \brief  Creates a binary operator object of a specified type.
+      *  \param  pLhs            A pointer to the expression object, which shall be on the left-hand-side.
+      *  \param  pRhs            A pointer to the expression object, which shall be on the right-hand-side.
+      *  \param  eOperatorKind   The type of the binary operator.
+      *  \param  crReturnType    The return type of the operator expression. */
+    ::clang::BinaryOperator*      CreateBinaryOperator(::clang::Expr *pLhs, ::clang::Expr *pRhs, ::clang::BinaryOperatorKind eOperatorKind, const ::clang::QualType &crReturnType);
+
+    /** \brief  Creates a binary operator object which represents a "less than" comparison.
+      *  \param  pLhs  A pointer to the expression object, which shall be on the left-hand-side.
+      *  \param  pRhs  A pointer to the expression object, which shall be on the right-hand-side. */
+    ::clang::BinaryOperator*      CreateBinaryOperatorLessThan(::clang::Expr *pLhs, ::clang::Expr *pRhs);
+
+    /** \brief  Wraps a statement object into a compound statement object.
+      *  \param  pStatement  A pointer to the statement object, which shall be encapsulated into an compound statement. */
+    ::clang::CompoundStmt*        CreateCompoundStatement(::clang::Stmt *pStatement);
+
+    /** \brief  Constructs a compound statement object around a vector of statement objects.
+      *  \param  crvecStatements   A reference to the statement vector. */
+    ::clang::CompoundStmt*        CreateCompoundStatement(const StatementVectorType &crvecStatements);
+
+    /** \brief  Constructs a declaration reference expression which points to a specific declaration.
+      *  \param  pValueDecl  A pointer to the value declaration object. */
+    ::clang::DeclRefExpr*         CreateDeclarationReferenceExpression(::clang::ValueDecl *pValueDecl);
+
+    /** \brief  Constructs a declaration statement for a specific declaration.
+      *  \param  pDeclRef  A pointer to a declaration reference expression object which points to the specific declaration. */
+    ::clang::DeclStmt*            CreateDeclarationStatement(::clang::DeclRefExpr *pDeclRef);
+
+    /** \brief  Constructs a declaration statement for a specific declaration.
+      *  \param  pValueDecl  A pointer to the value declaration object. */
+    ::clang::DeclStmt*            CreateDeclarationStatement(::clang::ValueDecl *pValueDecl);
+
+    /** \brief  Creates an implicit cast expression object.
+      *  \param  pOperandExpression  A pointer to the expression object whose return type shall be implicitly casted.
+      *  \param  crReturnType        The qualified return type of the cast.
+      *  \param  eCastKind           The internal kind of the cast.
+      *  \param  bIsLValue           Specifies, whether the implicit cast expression is used as a L-value of another expression. */
+    ::clang::ImplicitCastExpr*    CreateImplicitCastExpression(::clang::Expr *pOperandExpression, const ::clang::QualType &crReturnType, ::clang::CastKind eCastKind, bool bIsLValue = false);
+
+    /** \brief  Creates an integer literal expression (i.e. a compile time constant).
+      *  \param  iValue  The value of the integer literal. */
+    ::clang::IntegerLiteral*      CreateIntegerLiteral(int32_t iValue);
+
+    /** \brief  Creates a parenthesis expression around another expression.
+      *  \param  pSubExpression  A pointer to the expression object which shall be encapsulated into a parenthesis expression. */
+    ::clang::ParenExpr*           CreateParenthesisExpression(::clang::Expr *pSubExpression);
+
+    /** \brief  Constructs a post increment statement for a declaration reference expression object.
+      *  \param  pDeclRef  A pointer to the declaration reference expression, which shall be used in the post increment operator. */
+    ::clang::UnaryOperator*       CreatePostIncrementOperator(::clang::DeclRefExpr *pDeclRef);
+
+    /** \brief    Creates a new variable declaration object.
+      *  \param    pParentFunction     A pointer to the function declaration object in whose context the new variable shall be declared.
+      *  \param    crstrVariableName   The name of the newly declared variable.
+      *  \param    crVariableType      The qualified type of newly declared variable.
+      *  \param    pInitExpression     A pointer to the initialization expression object for the variable declaration (i.e. the R-value of the assignment).
+      *  \remarks  The created variable declaration is automatically added to the declaration context of the specified function declaration. */
+    ::clang::VarDecl*             CreateVariableDeclaration(::clang::FunctionDecl *pParentFunction, const std::string &crstrVariableName, const ::clang::QualType &crVariableType, ::clang::Expr *pInitExpression);
+
+    //@}
+
+  public:
+
+
+    /** \brief  Counts the number of declaration references to a specific declaration inside a statement tree.
+      *  \param  pStatement          A pointer to the root of the statement tree which shall be parsed for the specified declaration references.
+      *  \param  crstrReferenceName  The name of the declaration reference whose appearances shall be counted. */
+    static unsigned int     CountNumberOfReferences(::clang::Stmt *pStatement, const std::string &crstrReferenceName);
+
+    /** \brief    Looks up a specific declaration.
+      *  \param    pFunction       A pointer to the function declaration object whose declaration context will be searched for the specified declaration.
+      *  \param    crstrDeclName   The name of the declaration which shall be searched for.
+      *  \return   If successful, a pointer to a newly created declaration reference expression for the found declaration, and zero otherwise. */
+    ::clang::DeclRefExpr*   FindDeclaration(::clang::FunctionDecl *pFunction, const std::string &crstrDeclName);
+
+    /** \brief  Checks whether a statement tree has only one branch (i.e. none of its nodes has more than one child).
+      *  \param  pStatement  A pointer to the root of the statement tree. */
+    static bool             IsSingleBranchStatement(::clang::Stmt *pStatement);
+
+    /** \brief  Replaces <b>all</b> instances of a declaration reference in a statement tree by a new value declaration.
+      *  \param  pStatement        A pointer to the root of the statement tree which shall be parsed for the specified declaration references.
+      *  \param  crstrDeclRefName  The name of the declaration reference which shall be replaced.
+      *  \param  pNewDecl          A pointer to the value declaration to which all reference will be updated. */
+    static void             ReplaceDeclarationReferences(::clang::Stmt* pStatement, const std::string &crstrDeclRefName, ::clang::ValueDecl *pNewDecl);
+  };
+} // end namespace Backend
+} // end namespace hipacc
+} // end namespace clang
+
+
+#endif  // _BACKEND_CLANG_AST_HELPER_H_
+
+// vim: set ts=2 sw=2 sts=2 et ai:
+
---
+++ lib/Backend/ClangASTHelper.cpp
@@ -0,0 +1,229 @@
+//
+// Copyright (c) 2012, University of Erlangen-Nuremberg
+// Copyright (c) 2012, Siemens AG
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// 1. Redistributions of source code must retain the above copyright notice, this
+//    list of conditions and the following disclaimer.
+// 2. Redistributions in binary form must reproduce the above copyright notice,
+//    this list of conditions and the following disclaimer in the documentation
+//    and/or other materials provided with the distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+// ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+// ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+// ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+
+//===--- ClangASTHelper.cpp - Implements helper class for easy clang AST handling. ---===//
+//
+// This file implements a helper class which contains a few methods for easy clang AST handling.
+//
+//===---------------------------------------------------------------------------------===//
+
+#include "hipacc/Backend/ClangASTHelper.h"
+
+using namespace clang::hipacc::Backend;
+using namespace clang;
+using namespace std;
+
+
+unsigned int ClangASTHelper::CountNumberOfReferences(Stmt *pStatement, const string &crstrReferenceName)
+{
+  if (pStatement == nullptr)
+  {
+    return 0;
+  }
+  else if (isa<::clang::DeclRefExpr>(pStatement))
+  {
+    DeclRefExpr *pCurrentDeclRef = dyn_cast<DeclRefExpr>(pStatement);
+
+    if (pCurrentDeclRef->getNameInfo().getAsString() == crstrReferenceName)
+    {
+      return 1;
+    }
+    else
+    {
+      return 0;
+    }
+  }
+  else
+  {
+    unsigned int uiChildRefCount = 0;
+
+    for (auto itChild = pStatement->child_begin(); itChild != pStatement->child_end(); itChild++)
+    {
+      uiChildRefCount += CountNumberOfReferences(*itChild, crstrReferenceName);
+    }
+
+    return uiChildRefCount;
+  }
+}
+
+
+ArraySubscriptExpr* ClangASTHelper::CreateArraySubscriptExpression(DeclRefExpr *pArrayRef, Expr *pIndexExpression, const QualType &crReturnType, bool bIsLValue)
+{
+  ExprValueKind  eValueKind = bIsLValue ? VK_LValue : VK_RValue;
+
+  return new (GetASTContext()) ArraySubscriptExpr(pArrayRef, pIndexExpression, crReturnType, eValueKind, OK_Ordinary, SourceLocation());
+}
+
+BinaryOperator* ClangASTHelper::CreateBinaryOperator(Expr *pLhs, Expr *pRhs, BinaryOperatorKind eOperatorKind, const QualType &crReturnType)
+{
+  return ASTNode::createBinaryOperator(GetASTContext(), pLhs, pRhs, eOperatorKind, crReturnType);
+}
+
+BinaryOperator* ClangASTHelper::CreateBinaryOperatorLessThan(Expr *pLhs, Expr *pRhs)
+{
+  return CreateBinaryOperator(pLhs, pRhs, BO_LT, GetASTContext().BoolTy);
+}
+
+CompoundStmt* ClangASTHelper::CreateCompoundStatement(Stmt *pStatement)
+{
+  StatementVectorType vecStatements;
+
+  vecStatements.push_back(pStatement);
+
+  return CreateCompoundStatement(vecStatements);
+}
+
+CompoundStmt* ClangASTHelper::CreateCompoundStatement(const StatementVectorType &crvecStatements)
+{
+  return ASTNode::createCompoundStmt(GetASTContext(), crvecStatements);
+}
+
+DeclRefExpr* ClangASTHelper::CreateDeclarationReferenceExpression(ValueDecl *pValueDecl)
+{
+  return ASTNode::createDeclRefExpr(GetASTContext(), pValueDecl);
+}
+
+DeclStmt* ClangASTHelper::CreateDeclarationStatement(DeclRefExpr *pDeclRef)
+{
+  return CreateDeclarationStatement(pDeclRef->getDecl());
+}
+
+DeclStmt* ClangASTHelper::CreateDeclarationStatement(ValueDecl *pValueDecl)
+{
+  return ASTNode::createDeclStmt(GetASTContext(), pValueDecl);
+}
+
+ImplicitCastExpr* ClangASTHelper::CreateImplicitCastExpression(Expr *pOperandExpression, const QualType &crReturnType, CastKind eCastKind, bool bIsLValue)
+{
+  ExprValueKind  eValueKind = bIsLValue ? VK_LValue : VK_RValue;
+
+  return ASTNode::createImplicitCastExpr(GetASTContext(), crReturnType, eCastKind, pOperandExpression, nullptr, eValueKind);
+}
+
+IntegerLiteral* ClangASTHelper::CreateIntegerLiteral(int32_t iValue)
+{
+  return ASTNode::createIntegerLiteral(GetASTContext(), iValue);
+}
+
+ParenExpr* ClangASTHelper::CreateParenthesisExpression(Expr *pSubExpression)
+{
+  return ASTNode::createParenExpr(GetASTContext(), pSubExpression);
+}
+
+UnaryOperator* ClangASTHelper::CreatePostIncrementOperator(DeclRefExpr *pDeclRef)
+{
+  return ASTNode::createUnaryOperator(GetASTContext(), pDeclRef, UO_PostInc, pDeclRef->getType());
+}
+
+VarDecl* ClangASTHelper::CreateVariableDeclaration(FunctionDecl *pParentFunction, const string &crstrVariableName, const QualType &crVariableType, Expr *pInitExpression)
+{
+  DeclContext *pDeclContext = FunctionDecl::castToDeclContext(pParentFunction);
+
+  VarDecl *pVarDecl = ASTNode::createVarDecl(GetASTContext(), pDeclContext, crstrVariableName, crVariableType, pInitExpression);
+  pDeclContext->addDecl(pVarDecl);
+
+  return pVarDecl;
+}
+
+
+DeclRefExpr* ClangASTHelper::FindDeclaration(FunctionDecl *pFunction, const string &crstrDeclName)
+{
+  DeclContext *pDeclContext = FunctionDecl::castToDeclContext(pFunction);
+
+  for (auto itDecl = pDeclContext->decls_begin(); itDecl != pDeclContext->decls_end(); itDecl++)
+  {
+    Decl *pDecl = *itDecl;
+
+    if ((pDecl == nullptr) || (!isa<ValueDecl>(pDecl)))
+    {
+      continue;
+    }
+
+    ValueDecl* pValueDecl = dyn_cast<ValueDecl>(pDecl);
+
+    if (pValueDecl->getNameAsString() == crstrDeclName)
+    {
+      return CreateDeclarationReferenceExpression(pValueDecl);
+    }
+  }
+
+  return nullptr;
+}
+
+bool ClangASTHelper::IsSingleBranchStatement(Stmt *pStatement)
+{
+  if (pStatement == nullptr)                                      // Empty statements have no children
+  {
+    return true;
+  }
+  else if (pStatement->child_begin() == pStatement->child_end())  // No children
+  {
+    return true;
+  }
+  else                                                            // Statement has children
+  {
+    // Check if more than one child is present
+    auto itChild = pStatement->child_begin();
+    itChild++;
+
+    if (itChild == pStatement->child_end())   // Only one child => Check if the child is a single branch
+    {
+      return IsSingleBranchStatement(*(pStatement->child_begin()));
+    }
+    else                                      // More than one child => Statement is not a single branch statement
+    {
+      return false;
+    }
+  }
+}
+
+void ClangASTHelper::ReplaceDeclarationReferences(Stmt* pStatement, const string &crstrDeclRefName, ValueDecl *pNewDecl)
+{
+  if (pStatement == nullptr)
+  {
+    return;
+  }
+  else if (isa<::clang::DeclRefExpr>(pStatement))
+  {
+    DeclRefExpr *pDeclRef = dyn_cast<DeclRefExpr>(pStatement);
+
+    if (pDeclRef->getNameInfo().getName().getAsString() == crstrDeclRefName)
+    {
+      pDeclRef->setDecl(pNewDecl);
+    }
+  }
+  else
+  {
+    for (auto itChild = pStatement->child_begin(); itChild != pStatement->child_end(); itChild++)
+    {
+      ReplaceDeclarationReferences(*itChild, crstrDeclRefName, pNewDecl);
+    }
+  }
+}
+
+
+// vim: set ts=2 sw=2 sts=2 et ai:
+
